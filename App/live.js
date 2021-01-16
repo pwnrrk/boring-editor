fca0ffbf6a004200dc294d74d0dc3ba027cda21c
R.Phuwanat <ggamegg170@gmail.com>
2021-01-16 17:12:41 +0700

    Hot fix live html

--- App/live.js
+++ App/live.js
@@ -4,7 +4,7 @@ import { ObjectHelper } from "./Helper/Object.helper.js"
 
 const url = new URLSearchParams(window.location.search)
 let files = JSON.parse(localStorage.getItem('files'))
-let content = ObjectHelper.find('id',url.get('file'),files.files).content
+let content = ObjectHelper.find('id',url.get('file'),files).content
 window.onload = ()=>{
     window.document.write(content)
 }
